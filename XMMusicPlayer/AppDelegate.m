5ecfa67e6186a76f6324d50ed6c75bc7a03b7e82
LoveCcheng <495737676@qq.com>
2016-12-13 17:18:44 +0800

    设置window

--- XMMusicPlayer/AppDelegate.m
+++ XMMusicPlayer/AppDelegate.m
@@ -17,14 +17,9 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     
-    
-    
-    
-    
-    
-    
-    
-    
+    self.window=[[UIWindow alloc]initWithFrame:[UIScreen mainScreen].bounds];
+    self.window.backgroundColor=[UIColor redColor];
+    [self.window makeKeyAndVisible];
     
     return YES;
 }
